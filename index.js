ff3a41d9130b631656bac3f12fca83ed681d5e6b
Steven Rosario <strosariogarcia@gmail.com>
2022-09-01 12:42:52 -0400

    commit tree bd4be0e00fe8fca46726aaeb36a9222b4e342237

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution here
+
+const findMatching = (driverArr, name) => {
+    const names = driverArr.filter((str)=>{
+        return str.toLowerCase().indexOf(name.toLowerCase()) >= 0
+    })
+    return names
+}
